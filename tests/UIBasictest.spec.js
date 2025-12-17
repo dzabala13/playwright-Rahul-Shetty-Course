ebcb1df05850694fc73dbd5d10f8b8a70b42e3eb
dzabala13 <danielisaaczabala@gmail.com>
2025-12-16 21:39:39 -0500

    Understanding broser context in playwright

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -1,5 +1,24 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import {  test, expect } from '@playwright/test';
+
+
+test('Understading hte browser context ', async ({ browser }) => {
+
+    // when you declare the following two lines, you are redefing the normal process that playwright makes
+    // behind the scenes, those configuration are specially helpfull when you want to customize 
+    // the context of your broswer for the test cases that you will run
+    // remember that you can customize the cookies the cache, proxy and a lot of different things doing this configuraciotn 
+
+
+    const browserContext = await browser.newContext();
+    const myPAge = await browserContext.newPage();
+
+
+    await myPAge.goto('https://google.com');
+
+
+});
+
 
 test.only('has title', async ({ page }) => {
   await page.goto('https://google.com');
