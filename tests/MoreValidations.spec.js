257b70a781988aed436e7900b16b45eec1c6aecc
dzabala13 <danielisaaczabala@gmail.com>
2026-03-05 11:41:46 -0500

    Visual comparition between a baseline image and the current status of your application
    also the process of taking screenshot of the whole page and a specific element

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -48,8 +48,36 @@ test("working with Iframes in playwirght", async ({page})=>{
 
     await frame.getByRole("link", {name : "All Access Plan"}).click();
     await frame.locator(".text h2").textContent().then(text => console.log(text));
+})
 
+test("Screenshots & Visual comparision", async ({page})=>{
 
+await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+await expect(page.locator("#displayed-text")).toBeVisible();
+// here you are taking a screenshot of an specific element using a locator and then using the method screenshot.
+await page.locator('#displayed-text').screenshot({path : 'partialScreenshot.png'})
+await page.locator("#hide-textbox").click();
+// in this part you are taking a screenshot of the whole page and it will be saved in the path that you define.
+await page.screenshot({path : 'screenshot.png'});
+await expect(page.locator("#displayed-text")).toBeHidden();
 
+})
+
+test('Visual comparision', async ({page}) => {
+// in this part you are making a comparation pyxel by pixel with a baseline images
+
+/** here there are some important aspect that you need to take in mind for this, the first 
+ * one is that:
+ * 
+ * The first time that you run the test, the test will fail since you dont have a baseline images which you can 
+ * use to compare the current status of your web page, so in this first time playwright will create a baseline imagen for this 
+ * 
+ * 
+ * The second thing that you need to have in mind is that:
+ * you need to be sure that anythign will change between your baseline imagen and the current status of your application since 
+ * doesn't matter if there is a small change for an animation of an clock that you have in your web, thta will make that the test fail.
+ */
+await page.goto("https://mail.google.com/mail/u/0/#inbox");
+expect(await page.screenshot()).toMatchSnapshot('landing.png')
 
 })
