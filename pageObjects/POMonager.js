f5bd5f5c207cfc091efc6371271464b6b0f5868a
dzabala13 <danielisaaczabala@gmail.com>
2026-03-05 17:53:01 -0500

    comments to understan page objects and POManager

--- pageObjects/POMonager.js
+++ pageObjects/POMonager.js
@@ -5,14 +5,19 @@ import { DashboardPage } from "./dashboardPage";
 class POManager{
 
 constructor(page){
-
+    /** here there are several importan things like we are using this._object to avoid confunsions 
+     * between the name of the object and the getters that we define later, beacuse that if you dont define 
+     * that property with the _name you will have a problem because when you call the getter it will try to call
+     *  the getter again and that will cause an infinite loop 
+     */
     this.page = page
     this._loginPage = new LoginPage(this.page);
     this._dashboardPage = new DashboardPage(this.page);
-
-
 }
 
+// here we are using setter since it is more easy understand the getter when you are using it into the test 
+// you just need to write POManager.loginPage, instead of POManager.loginPage(), and that is because the getter is a property.
+// and you are not  defining the children POM as a function.
 get loginPage(){
 return this._loginPage;
 }
@@ -25,4 +30,7 @@ get dashboardPage(){
 
 }
 
+// here we need to use the export sentence instead of module.export beacuse we are using the import/export syntax 
+// instead of the require/module.exports syntax, and that is because we are using ES6 modules instead of CommonJS
+//  modules
 export { POManager }
--- tests/E2EFlowWithPOM.spec.js
+++ tests/E2EFlowWithPOM.spec.js
@@ -1,10 +1,17 @@
 // @ts-check
-import {  test, expect } from '@playwright/test';
-import { LoginPage } from '../pageObjects/LoginPage';
-import { DashboardPage } from '../pageObjects/dashboardPage';
-import {POManager} from '../pageObjects/POMonager'
+import { test, expect } from '@playwright/test';
+import { POManager } from '../pageObjects/POMonager'
+/** remeber that for import the name of the object of module that you are importin should be the same that you define 
+ * as name of the class and the name that you use in the export sentence.
+ * 
+ * import { POManager } from '../pageObjects/POMonager'
+*       ^^^^^^^^^                    ^^^^^^^^^
+*    nombre de la clase/export       nombre del ARCHIVO
+*    (debe coincidir con             (ruta física del
+*    lo que está en module.exports)  archivo en disco)
+ */
 
-test('first end to end testing', async ({page}) => {
+test('first end to end testing', async ({ page }) => {
 
   const pomanager = new POManager(page);
 
@@ -13,52 +20,51 @@ test('first end to end testing', async ({page}) => {
   const producName = 'ZARA COAT 3'
 
   await pomanager.loginPage.goTo();
-  await pomanager.loginPage.validateLogin(username,password);
-
+  await pomanager.loginPage.validateLogin(username, password);
 
   await pomanager.dashboardPage.addProductTocart(producName);
   await pomanager.dashboardPage.goToCart();
-  
 
 
 
 
- //  await page.locator('.card-body')
- //   .filter({ hasText: producName }) // Simplest way to filter by text anywhere in the card
- //   .getByRole('button', { name: 'Add To Cart' })
- //   .click();
-//
- // await page.getByRole("listitem").getByRole("button",{name:"Cart"}).click();
-  
+
+  //  await page.locator('.card-body')
+  //   .filter({ hasText: producName }) // Simplest way to filter by text anywhere in the card
+  //   .getByRole('button', { name: 'Add To Cart' })
+  //   .click();
+  //
+  // await page.getByRole("listitem").getByRole("button",{name:"Cart"}).click();
+
   await page.locator("img.itemImg").first().waitFor();
-  
+
   await expect(page.getByText("ZARA COAT 3")).toBeVisible();
-  await page.getByRole("button",{name:"Checkout"}).click(); 
-  
-  await page.getByPlaceholder("Select Country").pressSequentially('Ind', {delay : 150});
-  await page.getByRole("button",{name:"India"}).last().click();
+  await page.getByRole("button", { name: "Checkout" }).click();
+
+  await page.getByPlaceholder("Select Country").pressSequentially('Ind', { delay: 150 });
+  await page.getByRole("button", { name: "India" }).last().click();
   await page.getByText("PLACE ORDER").click();
 
 
   await expect(page.getByText("Thankyou for the order.")).toBeVisible();
 
   //const orderID  = await page.locator('label.ng-star-inserted').textContent();
-  const orderID  = (await page.locator('label.ng-star-inserted').textContent()) ?? '';
+  const orderID = (await page.locator('label.ng-star-inserted').textContent()) ?? '';
 
   console.log(orderID);
 
   // here you can have que the li items using the role listitems and then filter the buttons by name that is so usefull
-await page.getByRole("listitem").getByRole("button", {name:"ORDERS"}).click();  
-await page.locator('table thead').waitFor();
+  await page.getByRole("listitem").getByRole("button", { name: "ORDERS" }).click();
+  await page.locator('table thead').waitFor();
 
 
-// gemini version 2
+  // gemini version 2
   await page.locator('tbody tr')
-  .filter({ has: page.getByText(orderID.replace(/[| ]/g, "")) })
-  .locator('td button')
-  .first()
-  .click();
+    .filter({ has: page.getByText(orderID.replace(/[| ]/g, "")) })
+    .locator('td button')
+    .first()
+    .click();
 
-console.log(await page.locator('div.col-text.-main').textContent())
+  console.log(await page.locator('div.col-text.-main').textContent())
 
-} )
+})
